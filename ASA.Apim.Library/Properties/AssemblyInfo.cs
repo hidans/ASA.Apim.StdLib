a8a017238582fdbbc96e15d6d3d2aee0a0e77c08
Bo Jacobsen <emetico@hotmail.com>
2018-02-04 10:57:27 +0100

    Fjernet da-DK til naturel
    Fjernet kultur info fra dansk til naturel.

--- ASA.Apim.Library/Properties/AssemblyInfo.cs
+++ ASA.Apim.Library/Properties/AssemblyInfo.cs
@@ -37,4 +37,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
-[assembly: NeutralResourcesLanguage("da-DK")]
+[assembly: NeutralResourcesLanguage("")]
